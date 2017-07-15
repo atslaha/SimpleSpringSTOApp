2058d25324dc5820bbc4db23a357e59c178332fc
artmeleshko <artem.meleshko@gmail.com>
2017-07-15 17:47:09 +0300

    The Second first Commit

--- src/main/java/Demo/Demo.java
+++ src/main/java/Demo/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
     public boolean getBool(){
         return true;
     }
+    
+    public boolean getBool2(){
+        return false;
+    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
